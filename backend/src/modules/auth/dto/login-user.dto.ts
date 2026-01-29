377044f3059c94361c1aa01694daed1ca6ca9329
beto <betooxx.dev@gmail.com>
2026-01-28 21:34:59 -0600

    feat(backend)/auth module documentation

--- backend/src/modules/auth/dto/login-user.dto.ts
+++ backend/src/modules/auth/dto/login-user.dto.ts
@@ -1,10 +1,21 @@
 import { IsEmail, IsString, IsStrongPassword, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginUserDto {
+    @ApiProperty({
+        description: 'The email of the user',
+        example: 'john.doe@example.com'
+    })
     @IsString()
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: 'The password of the user',
+        example: 'Secret123!',
+        minLength: 9,
+        maxLength: 20
+    })
     @IsString()
     @MinLength(9)
     @MaxLength(20)
--- backend/src/modules/auth/dto/register-user.dto.ts
+++ backend/src/modules/auth/dto/register-user.dto.ts
@@ -1,13 +1,30 @@
 import { IsEmail, IsOptional, IsString, IsStrongPassword, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterUserDto {
+    @ApiProperty({
+        description: 'The name of the user',
+        example: 'John Doe',
+        minLength: 1
+    })
     @IsString()
     name: string;
 
+    @ApiProperty({
+        description: 'The email of the user',
+        example: 'john.doe@example.com',
+        uniqueItems: true
+    })
     @IsString()
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: 'The password of the user',
+        example: 'Secret123!',
+        minLength: 9,
+        maxLength: 20
+    })
     @IsString()
     @MinLength(9)
     @MaxLength(20)
@@ -19,6 +36,12 @@ export class RegisterUserDto {
     })
     password: string;
 
+    @ApiProperty({
+        description: 'The role of the user (admin, user, super-user)',
+        example: 'user',
+        default: 'user',
+        required: false
+    })
     @IsOptional()
     @IsString()
     role: string;
